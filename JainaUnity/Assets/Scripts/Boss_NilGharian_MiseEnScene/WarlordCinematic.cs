ccf9896c7cb264c28807c5ce206c4e5058da1ca8
Jeremy_Laptop <jeremy.payre@live.fr>
2019-07-16 14:26:33 +0200

    Add move animation on WarlordCinematic.cs

--- JainaUnity/Assets/Scripts/Boss_NilGharian_MiseEnScene/WarlordCinematic.cs
+++ JainaUnity/Assets/Scripts/Boss_NilGharian_MiseEnScene/WarlordCinematic.cs
@@ -8,10 +8,16 @@ public class WarlordCinematic : MonoBehaviour {
     [SerializeField] Transform m_targetAgentPosition;
 
     NavMeshAgent m_agent;
+    Animator m_animator;
+
+    void Start(){
+        m_agent = GetComponent<NavMeshAgent>();
+        m_animator = GetComponentInChildren<Animator>();
+    }
 
     void OnEnable(){
-        m_agent = GetComponent<NavMeshAgent>();
         m_agent.SetDestination(m_targetAgentPosition.position);
+        m_animator.SetTrigger("Chase");
     }
 
 }
