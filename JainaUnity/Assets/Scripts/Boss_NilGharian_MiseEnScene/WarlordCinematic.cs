2049d4da48bb0ab59b7db02a8458a937604d163a
Jeremy_Laptop <jeremy.payre@live.fr>
2019-07-16 14:23:49 +0200

    Add WarlordCinematic.cs to move Warlord

---
+++ JainaUnity/Assets/Scripts/Boss_NilGharian_MiseEnScene/WarlordCinematic.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WarlordCinematic : MonoBehaviour {
+    
+    [SerializeField] Transform m_targetAgentPosition;
+
+    NavMeshAgent m_agent;
+
+    void OnEnable(){
+        m_agent = GetComponent<NavMeshAgent>();
+        m_agent.SetDestination(m_targetAgentPosition.position);
+    }
+
+}
