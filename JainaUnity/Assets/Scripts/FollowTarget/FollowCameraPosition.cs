36b7ae8b7b2e8cef619c9eeef20bdd4765ecc8a3
Jeremy_Laptop <jeremy.payre@live.fr>
2019-07-17 12:03:26 +0200

    Add new spell icons Add FollowCameraPosition.cs & FollowPlayerPosition.cs for Water & RayCastGround.prefab can follow camera & Player

---
+++ JainaUnity/Assets/Scripts/FollowTarget/FollowCameraPosition.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCameraPosition : MonoBehaviour {
+    
+    CameraManager m_cameraManager;
+    Transform m_targetTransform;
+
+    void Start(){
+        m_cameraManager = CameraManager.Instance;
+        m_targetTransform = m_cameraManager.transform;
+    }
+
+    void FixedUpdate(){
+        transform.position = new Vector3(m_targetTransform.position.x, transform.position.y, m_targetTransform.position.z);
+    }
+
+}
---
+++ JainaUnity/Assets/Scripts/FollowTarget/FollowPlayerPosition.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerPosition : MonoBehaviour {
+    
+    PlayerManager m_playerManager;
+    Transform m_targetTransform;
+
+    void Start(){
+        m_playerManager = PlayerManager.Instance;
+        m_targetTransform = m_playerManager.transform;
+    }
+
+    void FixedUpdate(){
+        transform.position = m_targetTransform.position;
+    }
+
+}
