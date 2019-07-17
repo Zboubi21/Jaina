f06b54f16f241d785b69e1e7b451243de5966c87
Zoulouti <Pauldelaubadere@gmail.com>
2019-07-17 19:36:55 +0200

    sound_commit

--- JainaUnity/Assets/Scripts/Boss_NilGharian_MiseEnScene/WarlordCinematic.cs
+++ JainaUnity/Assets/Scripts/Boss_NilGharian_MiseEnScene/WarlordCinematic.cs
@@ -6,6 +6,7 @@ using UnityEngine.AI;
 public class WarlordCinematic : MonoBehaviour {
     
     [SerializeField] Transform m_targetAgentPosition;
+    [SerializeField] float m_timeBeforeFleeing;
 
     NavMeshAgent m_agent;
     Animator m_animator;
@@ -15,6 +16,13 @@ public class WarlordCinematic : MonoBehaviour {
         m_animator = GetComponentInChildren<Animator>();
 
         m_agent.ResetPath();
+
+        StartCoroutine(WaitBeforeFleeing(m_timeBeforeFleeing));
+    }
+
+    IEnumerator WaitBeforeFleeing(float time)
+    {
+        yield return new WaitForSeconds(time);
         m_agent.SetDestination(m_targetAgentPosition.position);
         m_animator.SetTrigger("Chase");
     }
