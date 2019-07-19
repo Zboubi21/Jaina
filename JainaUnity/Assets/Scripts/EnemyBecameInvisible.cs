7cce4803cd50ebd713b55d50edbd03a5d547fa7a
Jeremy_Laptop <jeremy.payre@live.fr>
2019-07-19 18:24:48 +0200

    Improve EnemyBecameInvisible.cs

--- JainaUnity/Assets/Scripts/EnemyBecameInvisible.cs
+++ JainaUnity/Assets/Scripts/EnemyBecameInvisible.cs
@@ -8,15 +8,7 @@ public class EnemyBecameInvisible : MonoBehaviour {
     [SerializeField] float m_additionalTimer = 0;
     [SerializeField] UnityEvent m_onBecameInvisible;
 
-    bool m_isActive = true;
-    public bool IsActive{
-        get{
-            return m_isActive;
-        }
-        set{
-            m_isActive = value;
-        }
-    }
+    public bool m_isActive = true;
 
     void OnEnable(){
         StopAllCoroutines();
