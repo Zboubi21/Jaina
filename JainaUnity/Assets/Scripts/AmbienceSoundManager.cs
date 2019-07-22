abc864a92ddcdc673b3690c3942d9affff4881c5
Jeremy_Laptop <jeremy.payre@live.fr>
2019-07-22 16:34:12 +0200

    Create AmbienceSoundManager.cs and AmbienceSoundManager.prefab

---
+++ JainaUnity/Assets/Scripts/AmbienceSoundManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class AmbienceSoundManager : MonoBehaviour {
+    
+    #region Singleton
+
+	public static AmbienceSoundManager Instance;
+
+	void Awake(){
+		if(Instance == null){
+			Instance = this;
+            DontDestroyOnLoad(gameObject);
+		}else{
+			// Debug.LogError("Two instance of AmbienceSoundManager");
+			gameObject.SetActive(false);
+            Destroy(gameObject);
+		}
+	}
+
+#endregion Singleton
+
+}
