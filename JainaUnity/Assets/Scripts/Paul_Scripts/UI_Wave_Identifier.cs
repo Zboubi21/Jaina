ad37107fb9e677a20f376bf758df2a43a6e4f224
Zoulouti <Pauldelaubadere@gmail.com>
2019-07-24 18:35:59 +0200

    Timer

---
+++ JainaUnity/Assets/Scripts/Paul_Scripts/UI_Wave_Identifier.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UI_Wave_Identifier : MonoBehaviour
+{
+    public TextMeshProUGUI timerWave;
+    public TextMeshProUGUI waveCounter;
+    public TextMeshProUGUI maxWave;
+    public TextMeshProUGUI Chrono;
+    public Image redCloud;
+}
