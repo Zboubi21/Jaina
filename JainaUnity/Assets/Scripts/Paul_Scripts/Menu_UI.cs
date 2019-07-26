97c49c119c807e911ed685c0700db936c0f94d69
Zoulouti <Pauldelaubadere@gmail.com>
2019-07-26 12:47:36 +0200

    Menu_UI.cs

---
+++ JainaUnity/Assets/Scripts/Paul_Scripts/Menu_UI.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+
+public class Menu_UI : UI {
+
+    Button ordalie;
+    public GameObject warning;
+    public UnityEvent OnMouseHover;
+    public UnityEvent OnMouseExit;
+
+
+    private void Start()
+    {
+        ordalie = GetComponent<Button>();
+    }
+
+
+    public override void OnPointerEnter(PointerEventData eventData) {
+
+        if (!ordalie.interactable)
+        {
+            warning.SetActive(true);
+            OnMouseHover.Invoke();
+        }
+    }
+
+    public override void OnPointerExit(PointerEventData eventData) {
+
+        if (!ordalie.interactable)
+        {
+            warning.SetActive(false);
+            OnMouseExit.Invoke();
+        }
+    }
+
+}
