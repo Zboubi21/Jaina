ed7c6f78c442ad258091b8a0eca55f282c91945e
Zoulouti <Pauldelaubadere@gmail.com>
2019-12-14 22:46:22 +0100

    HealHandeler.cs

---
+++ JainaUnity/Assets/Scripts/Boss/Actif_Heal/HealHandeler.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealHandeler : MonoBehaviour
+{
+    ReferenceScript _getImg;
+
+    public int maxHealCount;
+    int _currentHealCount;
+
+    public float healCooldown;
+    float _currenthealCooldown;
+
+
+    private void Start()
+    {
+        _getImg = GetComponent<ReferenceScript>();
+        _currentHealCount = maxHealCount;
+    }
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            if(_currentHealCount -1 >= 0)
+            {
+                _currentHealCount--;
+            }
+        }
+
+        if (_currentHealCount < maxHealCount)
+        {
+            _currenthealCooldown += Time.deltaTime;
+
+            if(_currentHealCount > 0)
+            {
+                _getImg.marksArray[1].fillAmount = Mathf.InverseLerp(0, healCooldown, _currenthealCooldown);
+            }
+            else
+            {
+                _getImg.marksArray[1].fillAmount = 0;
+                _getImg.marksArray[0].fillAmount = Mathf.InverseLerp(0, healCooldown, _currenthealCooldown);
+            }
+
+            if (_currenthealCooldown >= healCooldown)
+            {
+                _currenthealCooldown = 0;
+                _currentHealCount++;
+            }
+        }
+        else
+        {
+            _currentHealCount = maxHealCount;
+        }
+
+        _getImg.count.text = string.Format("{0}", _currentHealCount);
+
+    }
+
+
+    void HealCoolDown()
+    {
+
+    }
+}
