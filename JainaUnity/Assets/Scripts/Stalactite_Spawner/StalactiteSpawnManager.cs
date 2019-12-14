9a6f59f11e399570c008487b5ba08ea6c4369b49
Zoulouti <Pauldelaubadere@gmail.com>
2019-12-14 14:28:28 +0100

    Boss.prefab

--- JainaUnity/Assets/Scripts/Stalactite_Spawner/StalactiteSpawnManager.cs
+++ JainaUnity/Assets/Scripts/Stalactite_Spawner/StalactiteSpawnManager.cs
@@ -1,25 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using PoolTypes;
 
 public class StalactiteSpawnManager : MonoBehaviour
 {
     public Transform[] possibleSlot;
-    [Space]
-    public GameObject stalactitePrefab;
+    //[Space]
+    //public GameObject stalactitePrefab;
     [Space]
     public int currentPhase;
+    [Space]
     public int[] nbrOfStalactitePerPhase;
+    public int[] nbrOfCristilizeStalactitePerPhase;
+    [Space]
+    [Range(0,100)]
+    public float chanceForAStalactiteToBeCristilized;
+    int _currentSpawnedStalactite;
+    int _currentCristilizeStalactite;
     [Space]
     public float minTimeBeforeStalactiteFall = 0f;
     public float maxTimeBeforeStalactiteFall = 1f;
     List<int> usedSlots = new List<int>();
     List<int> possibleSlotInts = new List<int>();
+    List<int> lavaSlots = new List<int>();
     int nbrOfFreeSlots;
     int randomSlots;
 
+    ObjectPooler m_objectPooler;
+
     private void Start()
     {
+        m_objectPooler = ObjectPooler.Instance;
+
         for (int i = 0, l = possibleSlot.Length; i < l; ++i)
         {
             possibleSlotInts.Add(i);
@@ -42,16 +55,16 @@ public class StalactiteSpawnManager : MonoBehaviour
 
             if(nbrOfFreeSlots >= nbrOfStalactitePerPhase[currentPhase])
             {
-                StartCoroutine(GetRandomNumber(nbrOfStalactitePerPhase[currentPhase]));
+                StartCoroutine(SpawnStalactite(nbrOfStalactitePerPhase[currentPhase]));
             }
             else
             {
-                StartCoroutine(GetRandomNumber(nbrOfFreeSlots));
+                StartCoroutine(SpawnStalactite(nbrOfFreeSlots));
             }
         }
     }
 
-    IEnumerator GetRandomNumber(int loop)
+    IEnumerator SpawnStalactite(int loop)
     {
         for (int i = 0, l = loop; i < l; ++i)
         {
@@ -59,8 +72,58 @@ public class StalactiteSpawnManager : MonoBehaviour
             yield return new WaitForSeconds(randomTime);
             int index = Random.Range(0, possibleSlotInts.Count);
             usedSlots.Add(possibleSlotInts[index]);
-            GameObject go = Instantiate(stalactitePrefab, possibleSlot[possibleSlotInts[index]].position, Quaternion.identity);
+
+
+            float isCristilize = Random.Range(0, 100);
+
+            if ((_currentCristilizeStalactite != nbrOfCristilizeStalactitePerPhase[currentPhase] && isCristilize <= chanceForAStalactiteToBeCristilized) || (nbrOfStalactitePerPhase[currentPhase] - _currentSpawnedStalactite == nbrOfCristilizeStalactitePerPhase[currentPhase]))
+            {
+                _currentCristilizeStalactite++;
+                GenerateStalactite(index, true);
+            }
+            else
+            {
+                _currentSpawnedStalactite++;
+                GenerateStalactite(index, false);
+            }
             possibleSlotInts.RemoveAt(index);
         }
+
+        _currentCristilizeStalactite = 0;
+
+    }
+
+    void GenerateStalactite(int index, bool hasToCristilize)
+    {
+        GameObject go = m_objectPooler.SpawnEnemyFromPool(EnemyType.Stalactite, possibleSlot[possibleSlotInts[index]].position, Quaternion.identity);
+
+        StalactiteSpawnerTracker tracker = go.AddComponent<StalactiteSpawnerTracker>(); //Tant que JJ boss sur le prefab
+
+        tracker.intSlotPosition = possibleSlotInts[index];
+        tracker.spawnManager = this;
+        tracker.isCristilize = hasToCristilize;
+
+        if(lavaSlots.Count != 0)
+        {
+            for (int i = 0, l = lavaSlots.Count; i < l; ++i)
+            {
+                if(possibleSlotInts[index] == lavaSlots[i])
+                {
+                    tracker.isInLava = true;
+                    break;
+                }
+                tracker.isInLava = false;
+            }
+        }
+    }
+
+    public void StalactiteHasBeenDestroyed(int pos, bool hasExploded)
+    {
+        if (hasExploded)
+        {
+            lavaSlots.Add(pos);
+        }
+        possibleSlotInts.Add(pos);
+        usedSlots.Remove(pos);
     }
 }
---
+++ JainaUnity/Assets/Scripts/Stalactite_Spawner/StalactiteSpawnerTracker.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StalactiteSpawnerTracker : MonoBehaviour
+{
+    public int intSlotPosition;
+    public StalactiteSpawnManager spawnManager;
+    public bool isCristilize;
+    public bool isInLava;
+
+    private void OnDisable()
+    {
+        Debug.Log("AIEU !!");
+        spawnManager.StalactiteHasBeenDestroyed(intSlotPosition, true);
+    }
+}
