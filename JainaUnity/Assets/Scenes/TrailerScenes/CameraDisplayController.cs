5c489ebc54e1b58ed5e236a33464ada7272a0d49
Zoulouti <Pauldelaubadere@gmail.com>
2020-02-29 21:25:43 +0100

    controller camera triple strike

---
+++ JainaUnity/Assets/Scenes/TrailerScenes/CameraDisplayController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraDisplayController : MonoBehaviour
+{
+    public KeyCode input;
+    Camera cam;
+
+    bool toggleOn;
+    private void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+    private void Update()
+    {
+        if(cam != null)
+        {
+            if (Input.GetKeyDown(input) && !toggleOn)
+            {
+                cam.enabled = true;
+                toggleOn = true;
+            }
+            else if(Input.GetKeyDown(input) && toggleOn)
+            {
+                cam.enabled = false;
+                toggleOn = false;
+            }
+        }
+    }
+}
